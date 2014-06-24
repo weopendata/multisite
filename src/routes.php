f89067034870add616a422bd827ef7f4eaa9338d
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2014-06-24 10:45:42 +0200

    Small route update

--- src/routes.php
+++ src/routes.php
@@ -5,9 +5,14 @@
  * Hijack all of the routes
  */
 
-Route::any('{site}/api/admin{api}', 'Tdt\Multisite\Controllers\BaseController@api')
+Route::any('{site}/api/admin', function ($site) {
+    return Redirect::to($site . '/api/admin/datasets');
+})
+->where('site', '[^\/]+');
 
-->where(array('url' => '[^\/]+', 'api' => '.*'));
+Route::any('{site}/api/admin/{api}', 'Tdt\Multisite\Controllers\BaseController@api')
+
+->where(array('site' => '[^\/]+', 'api' => '.*'));
 
 Route::any('{full_url}', 'Tdt\Multisite\Controllers\BaseController@handle')
 
